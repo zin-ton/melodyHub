bae2a49ad219dbdc3e0629262cbaa9ebc298887b
llutsefer <n.kuziv2005@gmail.com>
2025-03-15 16:37:53 +0100

    Add Swagger

---
+++ src/main/java/com/backend/melodyHub/MelodyHubApplication.java
@@ -0,0 +1,13 @@
+package com.backend.melodyHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodyHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelodyHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/melodyHub/MelodyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.melodyHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelodyHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
