58d7916561f1ab15931cc841dc6144c5d1eeb251
zinton <anton.zhylitski@gmail.com>
2025-04-07 15:38:46 +0200

    fixed bugs verification of data

--- src/main/java/com/backend/melodyHub/repository/UserRepository.java
+++ src/main/java/com/backend/melodyHub/repository/UserRepository.java
@@ -4,11 +4,12 @@ import com.backend.melodyHub.model.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByLogin(String login);
-
+    @Validated
     Optional<User> findByEmail(@NotBlank(message = "Email cannot be empty") @Email(message = "Invalid email format") String email);
 }
