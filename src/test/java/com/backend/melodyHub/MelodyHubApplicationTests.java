155459306552b5c42b9d2e8e67e70d764c9ec944
llutsefer <n.kuziv2005@gmail.com>
2025-06-20 15:56:40 +0200

    Fix contextLoads test

--- src/test/java/com/backend/melodyHub/MelodyHubApplicationTests.java
+++ src/test/java/com/backend/melodyHub/MelodyHubApplicationTests.java
@@ -1,13 +1,28 @@
 package com.backend.melodyHub;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MelodyHubApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeAll
+    static void init() {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("256BIT_SECRET", dotenv.get("256BIT_SECRET"));
+        System.setProperty("BUCKET_NAME", dotenv.get("BUCKET_NAME"));
+        System.setProperty("AWS_ACCESS_KEY", dotenv.get("AWS_ACCESS_KEY"));
+        System.setProperty("AWS_SECRET_KEY", dotenv.get("AWS_SECRET_KEY"));
+        System.setProperty("AWS_REGION", dotenv.get("AWS_REGION"));
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
