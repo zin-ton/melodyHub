608a90a44f56fd685d0797177ddd5394240bdce0
llutsefer <n.kuziv2005@gmail.com>
2025-06-21 23:30:38 +0200

    Fix CorsConfig

--- src/main/java/com/backend/melodyHub/configs/CorsConfig.java
+++ src/main/java/com/backend/melodyHub/configs/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.backend.melodyHub.configs;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,13 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${cors.allowedOrigin}")
-    private String allowedOrigin;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(allowedOrigin)
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*").allowCredentials(false);
     }
 }
+
